33bf3a234f5544158e83c76499bcb27d0156a3d5
ivy <ib>
2026-04-01 23:58:06 -0700

    done w hw7

--- ivanashashi/homework2/homework2.py
+++ ivanashashi/homework2/homework2.py
@@ -6,43 +6,48 @@
 # Questions (Answer these in the homework2.py file as comments):
 
 # 1) What’s the difference between Git, GitHub, and Git Bash?
-
+#Core version control, website, command line interface
 # 2) What’s the difference between the terminal and the command line?
-
+# terminal is the software application, command line is you type commands
 # 3) How does Windows PowerShell differ from Git Bash?
-
+#power shell is object oriented, git is text based
 # 4) What’s the difference between Anaconda, conda, and Python?
+#anoconda is a software thing that Is used to use python t carry out projects, python is the language, condo is tool used in anaconda distrubutin 
 
 # 5) What is VS Code? 
-
+# VS code is an app like anaconda where its a code editior 
 # 6) What is a Jupyter Notebook? How is it different from Jupyter Lab?
-
+# Lab is a modern version to the notebook 
 # 7) What does ~/ mean?
-
+#user home directory 
 # 8) What’s the difference between an absolute path and a relative path?
-
+#fixed location vs based on directory
 # 9) Imagine you're in your "yourname" repo. Write the absolute and relative paths to "course_assignments/homework2".
-
+# Absolute: cd 
 # 10) What command lets you move from "course_assignments/homework2/" to "course_assignments/"?
-
+# Cd
 # 11) What would rm ./ do in your current directory? (Don’t try it!)
-
+# Remove permanently 
 # 12) What do the following commands do?
-# git add
-# git commit
-# git push
+# git add - moves changes to stagin area 
 
-# 13) What's the difference between "git add ." and "git add <file>"?
+# git commit-makes a commit and saves to history
+# git push- upload local to remote repo
 
-# 14) What do "git status" and "git log -1" do?
+# 13) What's the difference between "git add ." and "git add <file>"?- the first one does ALL the files vs the second is specifica to tat file
 
-# 15) What’s the difference between cloning a repository and pulling from it?
+# 14) What do "git status" and "git log -1" do?- I think log is the history and status is the current thing in the staging area 
+
+# 15) What’s the difference between cloning a repository and pulling from it?-git clone croaks a copy of the rep, why pull updates local with the new remote changes
 
 # 16) What has been your most frustrating bug or error in this class so far? How did you troubleshoot or fix it?
+# FOR SURE GIT PUSH ORIGIN MAIN and passkeys. Im trying to fix it the main problem is that I do remember my password but t says  Password authentication is not supported for Git operations. Even though I know my password and enter it, it won't accept it
 
-# 17) What’s a question you still have? What’s something you’re confused about?
+# 17) What’s a question you still have? What’s something you’re confused about? Im still confused making changes in the actual GitHub after uploading local/remote 
 
 # 18) Tell me a fun fact!
-
+# I can wiggle my ears!
 # 19) Print your favorite math expression you've learned in Python so far. 
 # (Hint: Use print() and add a comment explaining what it does.)
+print(2%5)
+#  this is the mod arthimiatic and gives the remainder of the division
